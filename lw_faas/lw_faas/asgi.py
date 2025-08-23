4133cc506c27d42172fad7481ba99c0c22370966
kirankumarbandaru <251410505361@mujonline.edu.in>
2025-08-23 23:30:04 +0530

    Basic Setup

---
+++ lw_faas/lw_faas/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for lw_faas project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lw_faas.settings')
+
+application = get_asgi_application()
